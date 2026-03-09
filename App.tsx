1229ce4db756a346d82b13cd32d00a2c760e7c9e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 20:31:05 -0300

    feat: implementação do MVP secundário

--- App.tsx
+++ App.tsx
@@ -1,30 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+type Consulta = {
+  id: number;
+  paciente: string;
+  medico: string;
+  data: string;
+  status: "agendada" | "confirmada" | "cancelada" | "realizada";
+};
 export default function App() {
+  const [consulta, setConsulta] = useState<Consulta>({
+    id: 1,
+    paciente: "Carlos Andrade",
+    medico: "Dr. Roberto Silva",
+    data: "28/02/2026",
+    status: "agendada",
+  });
+  function confirmarConsulta() {
+    setConsulta({
+      ...consulta,
+      status: "confirmada",
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>2CCR</Text>
-      <StatusBar style="auto" />
-      <Text style={styles.subtitulo}>Olá</Text>
+      <Text style={styles.titulo}>Sistema de Consultas</Text>
+      <View style={styles.card}>
+        <Text>Paciente: {consulta.paciente}</Text>
+        <Text>Médico: {consulta.medico}</Text>
+        <Text>Data: {consulta.data}</Text>
+        <Text>Status: {consulta.status}</Text>
+        <Text style={styles.paragraph}>
+          {'\n'}
+        </Text>
+        {consulta.status === "agendada" && (
+          <Button title="Confirmar Consulta" onPress={confirmarConsulta} />
+        )}
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
   },
   titulo: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: 'white',
   },
-  subtitulo: {
-    color: '#fff',
-    fontSize: 20,
+  card: {
+    backgroundColor: 'white',
+    width: "80%",
+    padding: 20,
+    borderWidth: 1,
+    borderRadius: 8,
   },
 });
